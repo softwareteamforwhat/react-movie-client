83ba644b514fcc37d5aa318e40ec879bbaa25c26
Tzung-Lin <3407584434@qq.com>
2020-12-19 11:48:51 +0800

    收藏页面:将想看人数改为取消收藏按钮，并添加hover效果

--- src/pages/favorite/favorite.jsx
+++ src/pages/favorite/favorite.jsx
@@ -1,42 +1,41 @@
 import React from 'react';
 import Header from '../../components/header';
-import {Link} from 'react-router-dom';
-import {Col,Pagination,Row} from 'antd';
-
-require('./favorite.css');
+import { Link } from 'react-router-dom';
+import { Col, Pagination, Row, Button,message } from 'antd';
+import './favorite.css'
 
 
 export default class Favorite extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {movieList: []};
+        this.state = { movieList: [] };
         //this.setState({movieList:this.getFakeMovieList(1)})
-        
+
     }
 
-    onChange = (page , pageSize = 10) => {
+    onChange = (page, pageSize = 10) => {
         /*
         apiGetTopRank(page).then((res) => {
             console.log(res);
             this.setState({movieList: res.data});
         })
         */
-       this.setState({
-           movieList:this.getFakeMovieList(page),
-       })
-    }
-    componentDidMount(){
         this.setState({
-            movieList:this.getFakeMovieList(1),
+            movieList: this.getFakeMovieList(page),
+        })
+    }
+    componentDidMount() {
+        this.setState({
+            movieList: this.getFakeMovieList(1),
         });
-        
-        //TODO:用实际API取代
+
+        //TODO:添加获取用户收藏信息api
     }
 
     /**
      * 获取假的电影列表
      */
-    getFakeMovieList(page){
+    getFakeMovieList(page) {
         const movielist = [
             {
                 movieId: "1240838",
@@ -47,7 +46,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -61,7 +60,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -75,7 +74,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -89,7 +88,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -103,7 +102,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -117,7 +116,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -131,7 +130,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -145,7 +144,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -159,7 +158,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -173,7 +172,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -187,7 +186,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -201,7 +200,7 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
@@ -215,96 +214,100 @@ export default class Favorite extends React.Component {
                 area: "中国香港,中国大陆",
                 length: "95分钟",
                 time: "2020-11-20中国大陆上映",
-                actors:[
+                actors: [
                     '王千源',
                     '春夏'
                 ],
             }
         ];
-        console.log('FakeData:'+this);
-        if(page===1){
-            return movielist.slice(0,10);
-        }else{
-            return movielist.slice(10,13);
+        console.log('FakeData:' + this);
+        if (page === 1) {
+            return movielist.slice(0, 10);
+        } else {
+            return movielist.slice(10, 13);
         }
-        
+
     }
 
     movieListRender = () => {
-        const list= this.state.movieList;
+        const list = this.state.movieList;
+        const cancelFavorite=()=>{
+            message.success('取消收藏电影成功');
+            //TODO:添加取消收藏api
+        }
         return (
 
             <Row>
                 {list.map((movie, index) => {
-                        const size = index+1 <= 2 ? "800%" : "400%";
-                        const color = index+1 <= 2 ? "#FFB400" : "black";
-                        return (<Col span={24} key={index}>
-                            <Link to={{
-                                pathname: "/movieinfo",
-                                state: {
-                                    id: movie.movieId
-                                }
-                            }}>
+                    const size = index + 1 <= 2 ? "800%" : "400%";
+                    const color = index + 1 <= 2 ? "#FFB400" : "black";
+                    return (<Col span={24} key={index} className='favorite-item'>
+                        <Link to={{
+                            pathname: "/movieinfo",
+                            state: {
+                                id: movie.movieId
+                            }
+                        }}>
 
-                                <Row gutter={[8, 8]}>
-                                    <Col span={2}/>
-                                    <Col span={2} style={{
-                                        display: "flex",
-                                        alignItems: "center"
-                                    }}> <span style={{
+                            <Row gutter={[8, 8]}>
+                                <Col span={2} />
+                                <Col span={2} style={{
+                                    display: "flex",
+                                    alignItems: "center"
+                                }}> <span style={{
 
-                                        fontWeight: "bold",
-                                        color: color,
-                                        fontStyle: "italic",
-                                        fontSize: size,
+                                    fontWeight: "bold",
+                                    color: color,
+                                    fontStyle: "italic",
+                                    fontSize: size,
 
-                                    }}></span></Col>
-                                    <Col span={6} style={{
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        alignItems: "center",
-                                        textAlign: "center",
-                                        height: "250px"
-                                    }}>
-                                        <img style={{width: "160px", height: "220px"}} alt={movie.name + "海报"}
-                                             src={movie.picture}/>
-                                    </Col>
-                                    <Col span={8} style={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                        justifyContent: "center",
-                                        textAlign: "center"
-                                    }}>
-                                        <h2 style={{color: "black", textAlign: "left"}}>{movie.name}</h2>
-                                        <span style={{color: "black", textAlign: "left"}}>{"类型：" + movie.type}</span>
-                                        <span style={{color: "black", textAlign: "left"}}>{"时长：" + movie.length}</span>
-                                        <span style={{color: "black", textAlign: "left"}}>{"主演：" + movie.actors.map((actor,index)=>{
-                                            if((index+1)===movie.actors.length){
-                                                return actor;
-                                            }else{
-                                                return actor;
-                                            }
-                                        })}</span>
-                                        <span style={{color: "black", textAlign: "left"}}>{"地区：" + movie.place}</span>
-                                        <span style={{color: "gray", textAlign: "left"}}>{"上映时间：" + movie.time}</span>
-                                        
-                                    </Col>
-                                    <Col span={4} style={{
-                                        display: "flex",
-                                        justifyContent: "flex-end",
-                                        alignItems: "center",
-                                        textAlign: "center"
-                                    }}>
-                                        <span style={{
-                                            textAlign: "center",
-                                            lineHeight: "100%",
-                                            color: "#FFB400"
-                                        }}>{parseInt(Math.random()*100)}人想看</span> </Col>
-                                    <Col span={2}/>
-                                </Row>
-                            </Link>
-                        </Col>)
-                    }
+                                }}></span></Col>
+                                <Col span={6} style={{
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    textAlign: "center",
+                                    height: "250px"
+                                }}>
+                                    <img style={{ width: "160px", height: "220px" }} alt={movie.name + "海报"}
+                                        src={movie.picture} />
+                                </Col>
+                                <Col span={8} style={{
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "center",
+                                    textAlign: "center"
+                                }}>
+                                    <h2 style={{ color: "black", textAlign: "left" }}>{movie.name}</h2>
+                                    <span style={{ color: "black", textAlign: "left" }}>{"类型：" + movie.type}</span>
+                                    <span style={{ color: "black", textAlign: "left" }}>{"时长：" + movie.length}</span>
+                                    <span style={{ color: "black", textAlign: "left" }}>{"主演：" + movie.actors.map((actor, index) => {
+                                        if ((index + 1) === movie.actors.length) {
+                                            return actor;
+                                        } else {
+                                            return actor;
+                                        }
+                                    })}</span>
+                                    <span style={{ color: "black", textAlign: "left" }}>{"地区：" + movie.area}</span>
+                                    <span style={{ color: "gray", textAlign: "left" }}>{"上映时间：" + movie.time}</span>
+
+                                </Col>
+                                <Col span={4} style={{
+                                    display: "flex",
+                                    justifyContent: "flex-end",
+                                    alignItems: "center",
+                                    textAlign: "center"
+                                }}>
+                                    {/*FIXME: Button按钮会被电影跳转链接覆盖到*/}
+                                    <Button htmlType="buttton" onClick={cancelFavorite}>
+                                        取消收藏
+                                    </Button>
+                                </Col>
+                                <Col span={2} />
+                            </Row>
+                        </Link>
+                    </Col>)
+                }
                 )}
             </Row>
         );
@@ -313,11 +316,11 @@ export default class Favorite extends React.Component {
     render() {
         return (
             <div>
-                <Header index={3}/>
+                <Header index={3} />
                 <h1>收藏电影</h1>
-                <div className="rank-top-content" style={{textAlign: "center"}}>
-                    <this.movieListRender/>
-                    <Pagination showSizeChanger={false} defaultCurrent={1} total={13} onChange={this.onChange}/>
+                <div className="rank-top-content" style={{ textAlign: "center" }}>
+                    <this.movieListRender />
+                    <Pagination showSizeChanger={false} defaultCurrent={1} total={13} onChange={this.onChange} />
                 </div>
             </div>
         );
