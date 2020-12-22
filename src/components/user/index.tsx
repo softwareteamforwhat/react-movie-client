f534ea4ec0974f5daa5c1ee07b31fb92220b7a90
AlexHSL <2825883930@qq.com>
2020-12-22 12:37:40 +0800

    refactor(用户菜单): 增加菜单消失时延，优化体验

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -5,64 +5,64 @@ import {Menu, Dropdown, Button} from 'antd';
 import {UserOutlined} from '@ant-design/icons';
 
 const Logout = () => {
-    localStorage.removeItem("id");
-    localStorage.removeItem("token");
-    localStorage.removeItem("avatar");
+  localStorage.removeItem("id");
+  localStorage.removeItem("token");
+  localStorage.removeItem("avatar");
 }
 
 const login_menu = (
-    <Menu>
-        <Menu.Item>
-            <a href="/user/orders">
-                我的订单
-            </a>
-        </Menu.Item>
-        <Menu.Item>
-            <a href="/user/info">
-                基本信息
-            </a>
-        </Menu.Item>
-        <Menu.Item>
-            <a href="/" onClick={Logout}>
-                退出登录
-            </a>
-        </Menu.Item>
-    </Menu>
+  <Menu subMenuCloseDelay={0.5}>
+    <Menu.Item>
+      <a href="/user/orders">
+        我的订单
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a href="/user/info">
+        基本信息
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a href="/" onClick={Logout}>
+        退出登录
+      </a>
+    </Menu.Item>
+  </Menu>
 );
 const logout_menu = (
-    <Menu>
-        <Menu.Item>
-            <a href="/login">
-                登录
-            </a>
-        </Menu.Item>
-    </Menu>
+  <Menu subMenuCloseDelay={0.5}>
+    <Menu.Item>
+      <a href="/login">
+        登录
+      </a>
+    </Menu.Item>
+  </Menu>
 );
 export default class User extends Component {
-    render() {
-        const avatar: string = localStorage.getItem("avatar") || userDefault;
-        const token = localStorage.getItem("token");
-        const user_avator = <img src={avatar} alt="头像"/>;
-        if (token) {
-            return (
-                <div onClick={() => {
-                    window.location.href = "/user/info"
-                }}>
-                    <Dropdown overlay={login_menu} placement="bottomCenter" arrow>
-                        <Button className="user-btn" shape="circle" icon={user_avator}/>
-                    </Dropdown>
-                </div>
-            );
-        } else {
-            return (
-                <div onClick={() => {
-                    window.location.href = "/login"
-                }}>
-                    <Dropdown overlay={logout_menu} placement="bottomCenter" arrow>
-                        <Button className="user-btn" shape="circle" icon={<UserOutlined/>}/>
-                    </Dropdown>
-                </div>
-            );
-        }
+  render() {
+    const avatar: string = localStorage.getItem("avatar") || userDefault;
+    const token = localStorage.getItem("token");
+    const user_avator = <img src={avatar} alt="头像"/>;
+    if (token) {
+      return (
+        <div onClick={() => {
+          window.location.href = "/user/info"
+        }}>
+          <Dropdown overlay={login_menu} placement="bottomCenter" arrow>
+            <Button className="user-btn" shape="circle" icon={user_avator}/>
+          </Dropdown>
+        </div>
+      );
+    } else {
+      return (
+        <div onClick={() => {
+          window.location.href = "/login"
+        }}>
+          <Dropdown overlay={logout_menu} placement="bottomCenter" arrow>
+            <Button className="user-btn" shape="circle" icon={<UserOutlined/>}/>
+          </Dropdown>
+        </div>
+      );
     }
+  }
 }
